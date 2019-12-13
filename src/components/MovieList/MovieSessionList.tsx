507d659ee67bcf683cc4415f80f6cbfe661cb0bb
Wojtek <wojtek.szczepaniak@webedia.co.uk>
2019-12-13 11:34:33 +0000

    updates

--- src/components/MovieList/MovieSessionList.tsx
+++ src/components/MovieList/MovieSessionList.tsx
@@ -1,36 +1,47 @@
-import * as React from "react";
+import * as React from 'react';
 import styled from 'styled-components';
-import {Times} from '../../types';
-import { darken } from 'polished'
+import { Times } from '../../types';
+import { darken } from 'polished';
 
 interface SessionListProps {
-  date: string,
-  times?: Times[]
+  date: string;
+  times?: Times[];
 }
 
 interface SingleSessionProps {
-  soldOut: boolean,
-  notBookable: boolean,
-  sessionExpired: boolean
+  soldOut: boolean;
+  notBookable: boolean;
+  sessionExpired: boolean;
 }
 
-
-const SingleSession = styled("button")<SingleSessionProps>`
-  border-radius:5px;
-  border:transparent;
-  background: ${props => (props.notBookable || props.sessionExpired || props.soldOut ? props.theme.btnColorSecondary : props.theme.btnColorPrimary)};
-  color: ${props => (props.notBookable || props.sessionExpired || props.soldOut ? props.theme.btnColorPrimary : props.theme.btnColorSecondary)};
-  padding:0.5rem;
-  cursor: ${props => (props.notBookable || props.sessionExpired || props.soldOut ? '' : 'pointer')};
+const SingleSession = styled('button')<SingleSessionProps>`
+  border-radius: 5px;
+  border: transparent;
+  background: ${props =>
+    props.notBookable || props.sessionExpired || props.soldOut
+      ? props.theme.btnColorSecondary
+      : props.theme.btnColorPrimary};
+  color: ${props =>
+    props.notBookable || props.sessionExpired || props.soldOut
+      ? props.theme.btnColorPrimary
+      : props.theme.btnColorSecondary};
+  padding: 0.5rem;
+  cursor: ${props =>
+    props.notBookable || props.sessionExpired || props.soldOut
+      ? ''
+      : 'pointer'};
   :hover {
-    background: ${props => (props.notBookable || props.sessionExpired || props.soldOut ? '' : darken(0.1, props.theme.btnColorPrimary))};
+    background: ${props =>
+      props.notBookable || props.sessionExpired || props.soldOut
+        ? ''
+        : darken(0.1, props.theme.btnColorPrimary)};
   }
 `;
 
 SingleSession.defaultProps = {
   theme: {
     btnColorPrimary: '#000',
-    btnColorSecondary: '#fff',
+    btnColorSecondary: '#fff'
   }
 };
 
@@ -38,23 +49,22 @@ const MovieSessionList: React.FunctionComponent<SessionListProps> = props => {
   return (
     <div>
       <h4>{props.date}</h4>
-      <div className="grid grid--4-col mt-1">
-         {props.times &&
+      <div className='grid grid--4-col mt-1'>
+        {props.times &&
           props.times.length > 0 &&
           props.times.map((singleSession, i) => (
             <SingleSession
               key={i}
               soldOut={singleSession.SoldOut}
               notBookable={singleSession.NotBookable}
-              sessionExpired={singleSession.SessionExpired}>
-              {singleSession.StartTime}     
+              sessionExpired={singleSession.SessionExpired}
+            >
+              {singleSession.StartTime}
             </SingleSession>
           ))}
       </div>
     </div>
   );
-}
-
-
+};
 
 export default MovieSessionList;
--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -1,31 +1,32 @@
-import * as React from "react";
+import * as React from 'react';
 import styled from 'styled-components';
-import {filmData} from '../../types';
+import { filmData } from '../../types';
 import MovieListItem from './MovieListItem';
 
 interface ListingProps {
-  data: filmData[]
+  data: filmData[];
 }
 
 const Container = styled.div`
-  margin:2rem auto;
-  width:600px; 
-  display:block;
+  margin: 2rem auto;
+  width: 600px;
+  display: block;
 `;
 
 const MovieList: React.FunctionComponent<ListingProps> = props => {
-    return (
-      <Container>
-        {props.data.map((singleFilm, i) => (
-             <MovieListItem
-              key={i}
-              title={singleFilm.Title}
-              poster={singleFilm.MediaItems.Poster}
-              sessions={singleFilm.Sessions}
-              filmId={singleFilm.FilmId}/>
-        ))}
-      </Container>
-    );
-  } 
+  return (
+    <Container>
+      {props.data.map((singleFilm, i) => (
+        <MovieListItem
+          key={i}
+          title={singleFilm.Title}
+          poster={singleFilm.MediaItems.Poster}
+          sessions={singleFilm.Sessions}
+          filmId={singleFilm.FilmId}
+        />
+      ))}
+    </Container>
+  );
+};
 
- export default MovieList;
+export default MovieList;
